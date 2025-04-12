173659c76e7efbfe638e5657af422a3e361cae08
Dylan Cheong Nam Kwong <dylancnk_97@hotmail.com>
2025-04-12 16:16:10 -0400

    correction 2

--- Jobboard-backend-supabase/src/config/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-    
-module.exports = {
-  port: process.env.PORT || 3000,
-  supabaseUrl: process.env.SUPABASE_URL,
-  supabaseKey: process.env.SUPABASE_ANON_KEY,
-  jwtSecret: process.env.JWT_SECRET || 'zXoOkHXfBY+Er+JV3F/0/iRJ/0VDWZk55D4dEYAPFz7I/iPeIHKeiJ6c7rwefgnkqk6A3YncT4MMXflByaIXIQ==',
-  environment: process.env.NODE_ENV || 'development',
-};
